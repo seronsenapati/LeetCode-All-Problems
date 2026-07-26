b34f40c357c9446e03368b355c316b450689c4f0
Seron Senapati <seronsenapati@gmail.com>
2026-07-26 18:44:21 +0530

    Time: 76 ms (30.95%), Space: 59 MB (83.37%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -0,0 +1,62 @@
+class Solution {    
+    public static void merge(int arr[] , int low , int mid , int high){
+        List<Integer> temp = new ArrayList<>();
+        int left = low , right = mid + 1;
+
+        while(left <= mid && right <= high){
+            if(arr[left] <= arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }else{
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        while(left <= mid){
+            temp.add(arr[left]);
+            left++;
+        }
+        while(right <= high){
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for(int i = low ; i <= high ; i++){
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+    public static int countPairs(int arr[] , int low , int mid , int high){
+        int cnt = 0; 
+        int right = mid + 1;
+
+        for(int i = low ; i <= mid ; i++){
+            while(right <= high && (long)arr[i] > 2L * arr[right]){
+                right++;
+            }
+            cnt += right - (mid + 1);
+        }
+
+        return cnt;
+    }
+
+    public static int mergeSort(int arr[] , int low , int high){
+        int cnt = 0;
+        if(low >= high){
+            return cnt;
+        }
+
+        int mid = low + (high - low) / 2;
+
+        cnt += mergeSort(arr , low , mid);
+        cnt += mergeSort(arr , mid + 1 , high);
+        cnt += countPairs(arr , low , mid , high);
+        merge(arr , low , mid , high);
+
+        return cnt;
+    }
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums , 0 , nums.length - 1);
+    }
+}
