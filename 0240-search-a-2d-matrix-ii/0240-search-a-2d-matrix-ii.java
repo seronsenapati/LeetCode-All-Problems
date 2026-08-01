358f9c6d96f9a6dbb06e9fb4feb70ef42fc9b14c
Seron Senapati <seronsenapati@gmail.com>
2026-08-01 13:30:27 +0530

    Time: 3 ms (88.4%), Space: 48.1 MB (65.64%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
         int row = matrix.length;   
         int col = matrix[0].length;
 
