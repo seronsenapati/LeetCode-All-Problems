83cd2d587cee337a2a6841774c0f8b581fa7e160
Seron Senapati <seronsenapati@gmail.com>
2026-08-02 13:27:52 +0530

    Time: 9 ms (13.82%), Space: 50 MB (5.29%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        Integer[][] dp = new Integer[n][n];
+
+        return solve(piles, 0, n - 1, dp) > 0;
+    }
+
+    private int solve(int[] piles, int left, int right, Integer[][] dp) {
+        if (left == right) {
+            return piles[left];
+        }
+
+        if (dp[left][right] != null) {
+            return dp[left][right];
+        }
+
+        int takeLeft = piles[left] - solve(piles, left + 1, right, dp);
+        int takeRight = piles[right] - solve(piles, left, right - 1, dp);
+
+        return dp[left][right] = Math.max(takeLeft, takeRight);
+    }
+}
